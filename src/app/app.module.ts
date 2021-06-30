948abe0f67342f5365c6663d23b25beee44bc39b
Nikola Gospodjinacki <ngogank@gmail.com>
2021-06-30 19:23:13 +0200

    Prvi deo frontenda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon'
+import {MatListModule} from '@angular/material/list';
+import { StatusComponent } from './status/status.component';
+import { FakultetComponent } from './fakultet/fakultet.component';
+import { DepartmanComponent } from './departman/departman.component';
+import { StudentComponent } from './student/student.component';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { AuthorComponent } from './core/author/author.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusComponent,
+    FakultetComponent,
+    DepartmanComponent,
+    StudentComponent,
+    HomeComponent,
+    AboutComponent,
+    AuthorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
